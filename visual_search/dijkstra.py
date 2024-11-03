edd4352319ed0496c97e1d05d7c6800d558e4a65
Felix <felix.morandau@gmail.com>
2024-11-03 22:51:22 +0200

    merge

--- visual_search/dijkstra.py
+++ visual_search/dijkstra.py
@@ -1,33 +1,43 @@
 from queue import PriorityQueue
 
-
 def dijkstra(problem):
     start_node = problem.get_start_spot()
+    goal_node = problem.get_goal_state()
+
     frontier = PriorityQueue()
     visited = set()
 
+    frontier.put((0, start_node))
     path = []
-    initial_cost = 0
-    frontier.put((initial_cost, (start_node, path)))
 
-    cost_so_far = {start_node: initial_cost}
+    cost_so_far = {start_node: 0}
+    parent_map = {start_node: None}
 
     while not frontier.empty():
-        current_cost, (curr_node, final_path) = frontier.get()
+        current_cost, curr_node = frontier.get()
 
-        if curr_node not in visited:
-            curr_node.make_closed()
-            visited.add(curr_node)
+        if curr_node in visited:
+            continue
 
-            for successor in problem.get_successors(curr_node):
-                new_cost = current_cost + 1
+        curr_node.make_closed()
+        visited.add(curr_node)
 
-                if successor not in visited or new_cost < cost_so_far.get(successor, float('inf')):
-                    if problem.is_goal_state(successor):
-                        return final_path + [successor]
+        if curr_node == goal_node:
+            step = curr_node
+            while step is not None:
+                path.append(step)
+                step = parent_map[step]
+            path.reverse()
 
-                    cost_so_far[successor] = new_cost
-                    successor.make_open()
-                    frontier.put((new_cost, (successor, final_path + [curr_node])))
+        for successor in problem.get_successors(curr_node):
+            new_cost = current_cost + 1
+
+            if successor not in visited or new_cost < cost_so_far.get(successor, float('inf')):
+                cost_so_far[successor] = new_cost
+                parent_map[successor] = curr_node
+                frontier.put((new_cost, successor))
+                successor.make_open()
 
         problem.draw_board()
+
+    return path
